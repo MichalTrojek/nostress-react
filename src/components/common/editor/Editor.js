0887983f6a9b00725a177a5c751127187efd7944
MichaTrojek <MichalTrojek1@gmail.com>
2020-11-21 23:23:25 +0100

    refactoring

--- src/components/common/editor/Editor.js
+++ src/components/common/editor/Editor.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import './Editor.css';
+import styled from 'styled-components';
+import Button from '../button/Button';
 
+const EditingArea = styled.div`
+  height: 20rem;
+`;
 class Editor extends React.Component {
   constructor(props) {
     super(props);
@@ -20,14 +25,19 @@ class Editor extends React.Component {
 
   render() {
     return (
-      <ReactQuill
-        theme="snow"
-        value={this.props.value}
-        onChange={this.props.onChange}
-        modules={this.modules}
-        formats={this.formats}
-        placeholder="Vložte nadpis a text k novince. Velikost fontu se nastaví sama podle obrazovky uživatele."
-      />
+      <>
+        <ReactQuill
+          theme="snow"
+          value={this.props.value}
+          onChange={this.props.onChange}
+          modules={this.modules}
+          formats={this.formats}
+          placeholder="Vložte nadpis a text k novince. Velikost fontu se nastaví sama podle obrazovky uživatele."
+        >
+          <EditingArea></EditingArea>
+        </ReactQuill>
+        <Button text="test"></Button>
+      </>
     );
   }
 }
