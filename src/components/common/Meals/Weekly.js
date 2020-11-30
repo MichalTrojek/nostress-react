f892814db62860f66fce729d66ccfa9b60afb15b
MichaTrojek <MichalTrojek1@gmail.com>
2020-11-30 19:49:22 +0100

    added Weekly menu

---
+++ src/components/common/Meals/Weekly.js
@@ -0,0 +1,12 @@
+const Weekly = () => {
+  const weeklyData = {};
+
+  return (
+    <>
+      <h1>Týdenní menu</h1>
+      <p></p>
+    </>
+  );
+};
+
+export default Weekly;
--- src/components/common/menu/Menu.js
+++ src/components/common/menu/Menu.js
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
+import Weekly from '../Meals';
 
+const MenuBackground = styled.div`
+  background-color: black;
+`;
 const MenuWrapper = styled.div`
   max-width: var(--max-width);
   margin: 0 auto;
 `;
 const MenuStyled = styled.section`
-  background-color: blue;
   height: 100vh;
 
   //padding: 8rem;
@@ -13,11 +16,13 @@ const MenuStyled = styled.section`
 
 const Menu = () => {
   return (
-    <MenuWrapper>
-      <MenuStyled id="menu">
-        <h1>Menu</h1>
-      </MenuStyled>
-    </MenuWrapper>
+    <MenuBackground>
+      <MenuWrapper>
+        <MenuStyled id="menu">
+          <Weekly></Weekly>
+        </MenuStyled>
+      </MenuWrapper>
+    </MenuBackground>
   );
 };
 
