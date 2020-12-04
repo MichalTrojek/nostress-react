4747bdc9350c070722308ff3d7fa203d1f1f5eb7
MichaTrojek <MichalTrojek1@gmail.com>
2020-12-04 12:13:50 +0100

    refactoring

--- src/components/common/PageLayout.js
+++ src/components/common/PageLayout.js
@@ -3,69 +3,6 @@ import styled from 'styled-components';
 
 const Content = styled.main`
   box-sizing: border-box;
-  // margin-top: 8rem;
-
-  h1,
-  h2,
-  h3,
-  h4 {
-    color: var(--color-primary);
-  }
-
-  @media only screen and (min-width: 320px) {
-    h1 {
-      font-size: 2rem;
-    }
-
-    p,
-    li {
-      font-size: 1.5rem;
-    }
-  }
-
-  @media only screen and (min-width: 360px) {
-    h1 {
-      font-size: 2.6rem;
-    }
-
-    p,
-    li {
-      font-size: 1.7rem;
-    }
-  }
-
-  @media only screen and (min-width: 411px) {
-    h1 {
-      font-size: 2.8rem;
-    }
-
-    p,
-    li {
-      font-size: 1.9rem;
-    }
-  }
-
-  @media only screen and (min-width: 768px) {
-    h1 {
-      font-size: 4rem;
-    }
-
-    p,
-    li {
-      font-size: 2.2rem;
-    }
-  }
-
-  @media only screen and (min-width: 1024px) {
-    h1 {
-      font-size: 7rem;
-    }
-
-    p,
-    li {
-      font-size: 2.5rem;
-    }
-  }
 `;
 
 export default function PageLayout({ children }) {
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -40,10 +40,71 @@ export const GlobalStyle = createGlobalStyle`
         font-family: 'Open Sans Condensed', sans-serif;
         color: var(--color-primary);
         text-rendering: optimizeLegibility;
+        } 
 
+
+        h1,
+  h2,
+  h3,
+  h4 {
+    color: var(--color-primary);
+  }
+
+  @media only screen and (min-width: 320px) {
+    h1 {
+      font-size: 2rem;
+    }
+
+    p,
+    li {
+      font-size: 1.5rem;
+    }
+  }
+
+  @media only screen and (min-width: 360px) {
+    h1 {
+      font-size: 2.6rem;
+    }
+
+    p,
+    li {
+      font-size: 1.7rem;
+    }
+  }
+
+  @media only screen and (min-width: 411px) {
+    h1 {
+      font-size: 2.8rem;
+    }
+
+    p,
+    li {
+      font-size: 1.9rem;
+    }
+  }
+
+  @media only screen and (min-width: 768px) {
+    h1 {
+      font-size: 4rem;
+    }
+
+    p,
+    li {
+      font-size: 2.2rem;
+    }
+  }
+
+  @media only screen and (min-width: 1024px) {
+    h1 {
+      font-size: 7rem;
+    }
+
+    p,
+    li {
+      font-size: 2.5rem;
+    }
+  }
 
         
-       
-        } 
     }
 `;
