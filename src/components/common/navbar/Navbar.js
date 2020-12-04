a6f774bff892a3e4c642b2e372544dfe7b2bf650
MichaTrojek <MichalTrojek1@gmail.com>
2020-12-04 20:50:42 +0100

    refactored folders

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -5,7 +5,7 @@ import logo from '../../../img/logo.png';
 
 import Burger from '../Burger';
 import SideMenuStyled from '../SideMenu';
-import NavBarMenu from '../NavBarMenu';
+import NavBarMenu from './NavBarMenu';
 
 const MENU_ITEMS = [
   { name: 'Domů', href: '/home' },
