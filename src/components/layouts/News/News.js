cd2410145a24726254dd4b5bbddcfe7feeadda10
MichaTrojek <MichalTrojek1@gmail.com>
2020-12-06 21:11:34 +0100

    added news section

---
+++ src/components/layouts/News/News.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import Wrapper from '../../common/Wrapper';
+import NewsCard from './NewsCard';
+
+import backgroundImg from '../../../img/news.png';
+
+const NewsBackground = styled.section`
+  background-image: url(${backgroundImg});
+  background-position: center;
+  background-size: cover;
+`;
+
+const NewsWrapper = styled(Wrapper)`
+  h1 {
+    padding-bottom: 2rem;
+  }
+`;
+
+const News = () => {
+  return (
+    <NewsBackground>
+      <NewsWrapper>
+        <h1>Novinky</h1>
+        <NewsCard />
+      </NewsWrapper>
+    </NewsBackground>
+  );
+};
+
+export default News;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,6 +5,7 @@ import Navbar from '../layouts/Navbar/';
 import Hero from '../layouts/Hero/';
 import MealsMenus from '../layouts/MealsMenus';
 import FoodAndBeverage from '../layouts/FoodAndBeverage';
+import News from '../layouts/News';
 import BussinessHours from '../layouts/BussinessHours';
 import Contact from '../layouts/Contact';
 import Footer from '../layouts/Footer';
@@ -16,6 +17,7 @@ const Home = () => {
       <Hero />
       <MealsMenus />
       <FoodAndBeverage />
+      <News />
       <BussinessHours />
       <Contact />
       <Footer />
