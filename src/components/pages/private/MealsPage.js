86d2bc64bcf53d19b1241642ca01e458f8ce19ae
MichaTrojek <MichalTrojek1@gmail.com>
2020-12-10 19:49:36 +0100

    fixed background i

--- src/components/pages/private/MealsPage.js
+++ src/components/pages/private/MealsPage.js
@@ -1,17 +1,22 @@
+import styled from 'styled-components';
 import Background from '../../common/Background';
 import Wrapper from '../../common/Wrapper';
 import MealsForms from '../../common/Forms/MealsForms';
 import MealsList from '../../common/Lists/MealsList';
 
+const MealsPageBackground = styled(Background)`
+  min-height: 100vh;
+`;
+
 const MealsPage = () => {
   return (
-    <Background>
+    <MealsPageBackground>
       <Wrapper>
         <h1>Tvorba menu</h1>
         <MealsForms />
         <MealsList />
       </Wrapper>
-    </Background>
+    </MealsPageBackground>
   );
 };
 
