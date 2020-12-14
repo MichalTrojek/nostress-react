d3db85ab4e48f4a906d5e926d85508e55398da34
MichaTrojek <MichalTrojek1@gmail.com>
2020-12-14 21:17:14 +0100

    added order summary section

--- src/components/layouts/public/Order/Cart/Cart.js
+++ src/components/layouts/public/Order/Cart/Cart.js
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 import { connect } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Button from '../../../../common/Button';
 
 const CartContainer = styled.div`
   border: 1px solid var(--color-tertiary);
+  Button {
+  }
 `;
 
 const CartItem = styled.div`
@@ -13,7 +16,8 @@ const CartItem = styled.div`
 
   .name {
     grid-row: 1 / span 1;
-    grid-column: 2 / span 10;
+    grid-column: 2 / span 9;
+    hyphens: auto;
   }
 
   .amount {
@@ -31,6 +35,7 @@ const CartHeader = styled.div`
   grid-template-columns: repeat(12, 1fr);
   padding: 0.2rem 1rem;
   border-bottom: 1px solid var(--color-tertiary);
+
   .name {
     grid-row: 1 / span 1;
     grid-column: 2 / span 10;
@@ -39,7 +44,6 @@ const CartHeader = styled.div`
   .amount {
     grid-row: 1 / span 1;
     grid-column: 1 / span 1;
-    /* padding-left: 0.3rem; */
   }
 
   .price {
@@ -48,7 +52,33 @@ const CartHeader = styled.div`
   }
 `;
 
+const CartFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  padding: 1rem;
+
+  .totalPrice {
+    font-weight: bold;
+    color: var(--color-tertiary);
+  }
+`;
+
 const Cart = ({ items = [] }) => {
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    let tempPrice = 0;
+    let tempAmount = 0;
+    items.forEach((item) => {
+      tempPrice += Number(item.price) * Number(item.amount);
+      tempAmount += Number(item.amount);
+    });
+    setTotalPrice(tempPrice);
+    setTotalAmount(tempAmount);
+  }, [items]);
+
   return (
     <CartContainer>
       <CartHeader>
@@ -56,7 +86,12 @@ const Cart = ({ items = [] }) => {
         <p className="name">Název</p>
         <p className="price">Cena/kus</p>
       </CartHeader>
-      {items.length > 0 ? renderItems() : <p>Prázdný</p>}
+      {renderItems()}
+      <CartFooter>
+        <p>Celkem kusů: {totalAmount}</p>
+        <p className="totalPrice">Celkem: {totalPrice} ,- </p>
+      </CartFooter>
+      <Button primary>Objednat</Button>
     </CartContainer>
   );
 
@@ -67,7 +102,6 @@ const Cart = ({ items = [] }) => {
           <p className="name">{item.name}</p>
           <p className="amount"> {item.amount}</p>
           <p className="price"> {item.price},-</p>
-          {/* <p>Celkem: {Number(item.price) * Number(item.amount)}</p> */}
         </CartItem>
       );
     });
