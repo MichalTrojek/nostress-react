2c15e9253be77c0dcaaa963056efd43718942664
MichaTrojek <MichalTrojek1@gmail.com>
2020-12-22 01:03:33 +0100

    fixed issue with news not being fetched

--- src/redux/actions/news/fetchNews.js
+++ src/redux/actions/news/fetchNews.js
@@ -3,7 +3,7 @@ import fetchNewsApiCall from '../../../api/news/fetchNewsApiCall';
 
 const fetchNews = () => {
   return async (dispatch, getState) => {
-    if (getState().news.length !== 0) {
+    if (getState().news.length === 0) {
       let data = await fetchNewsApiCall();
       dispatch({ type: FETCH_NEWS, payload: data });
     }
