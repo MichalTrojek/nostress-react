b5cf94174f7bc0b2086f558fda4b148b936ebb53
MichaTrojek <MichalTrojek1@gmail.com>
2020-12-23 19:11:50 +0100

    improved styling

--- src/components/layouts/private/CustomerOrders/OrderedItem/OrderedItem.js
+++ src/components/layouts/private/CustomerOrders/OrderedItem/OrderedItem.js
@@ -42,6 +42,7 @@ const OrderedItemContainer = styled.div`
     padding: 0 1rem 0 1rem;
     background-color: var(--color-quaternary);
     color: white;
+    font-weight: bold;
   }
 
   .information {
