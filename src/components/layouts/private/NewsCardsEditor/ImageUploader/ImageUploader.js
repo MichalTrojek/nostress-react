5b3fd4c999ac7c4569e4fd2ff1ba91222846e899
MichaTrojek <MichalTrojek1@gmail.com>
2020-12-26 13:04:38 +0100

    styling

--- src/components/layouts/private/NewsCardsEditor/ImageUploader/ImageUploader.js
+++ src/components/layouts/private/NewsCardsEditor/ImageUploader/ImageUploader.js
@@ -6,7 +6,7 @@ import { storage } from '../../../../../firebase';
 const UploaderContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 1rem;
+  margin-bottom: 3rem;
 
   input {
     width: 0.1px;
