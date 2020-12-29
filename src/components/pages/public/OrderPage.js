ac11cc62d023ab1a0f016c42592ad676db36c432
MichaTrojek <MichalTrojek1@gmail.com>
2020-12-29 11:01:33 +0100

    added navbar to order page

--- src/components/pages/public/OrderPage.js
+++ src/components/pages/public/OrderPage.js
@@ -7,21 +7,53 @@ import Wrapper from '../../common/Wrapper';
 import Button from '../../common/Button';
 
 import OrderBreakfastMenu from '../../layouts/public/Orders/OrderBreakfastMenu';
+
 import OrderContainer from '../../layouts/public/Orders/styles/OrderContainer';
 import OrderMainMenu from '../../layouts/public/Orders/OrderMainMenu';
 import Summary from '../../layouts/public/Orders/Summary';
 import Cart from '../../layouts/public/Orders/Cart';
 
-import { showInfoToast } from '../../../notifications/toast';
+import logo from '../../../img/logo.png';
+
+import { showErrorToast, showInfoToast } from '../../../notifications/toast';
 
 const OrderPageBackground = styled.section`
   background-color: black;
   min-height: 100vh;
 `;
 
+const GoBackNavBar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 3rem;
+  .GoBackNavBar__logo {
+    height: 5rem;
+    max-width: 100%;
+    transition: height 1s;
+    @media only screen and (min-width: 360px) {
+      height: 6rem;
+    }
+  }
+`;
+
+const GoBackNavBarItem = styled.div`
+  padding: 1rem;
+  border-radius: 0.5rem;
+  text-align: center;
+  font-size: 2.1rem;
+  font-weight: bold;
+  cursor: pointer;
+  &:hover {
+    background-color: var(--color-tertiary);
+    color: black;
+  }
+`;
+
 const OrderPage = ({ items, orderingStarted, resetOrders }) => {
   const [isOrderingAllowed, setIsOrderingAllowed] = useState(false);
   const [showSummary, setShowSummary] = useState(false);
+  const [showShorterText, setShowShorterText] = useState(false);
   const history = useHistory();
 
   useEffect(() => {
@@ -32,9 +64,34 @@ const OrderPage = ({ items, orderingStarted, resetOrders }) => {
     }
   }, [items, orderingStarted.status, history]);
 
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    function handleResize() {
+      if (window.innerWidth <= 570) {
+        setShowShorterText(true);
+      } else {
+        setShowShorterText(false);
+      }
+    }
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <OrderPageBackground>
-      <Wrapper>{showSummary ? renderSummary() : renderMenuPicker()}</Wrapper>
+      <Wrapper>
+        <GoBackNavBar>
+          <img className="GoBackNavBar__logo" src={logo} alt="no stress logo" />
+          <GoBackNavBarItem onClick={() => history.push('/')}>
+            {showShorterText ? 'ZPĚT' : 'VRÁTIT SE NA HLAVNÍ STRÁNKU'}
+          </GoBackNavBarItem>
+        </GoBackNavBar>
+        {showSummary ? renderSummary() : renderMenuPicker()}
+      </Wrapper>
     </OrderPageBackground>
   );
 
