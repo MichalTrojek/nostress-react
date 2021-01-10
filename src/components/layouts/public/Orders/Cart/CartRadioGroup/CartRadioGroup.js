a50ad8e28341a32cd168aa701f1b61b64da80cb5
MichaTrojek <MichalTrojek1@gmail.com>
2021-01-10 14:53:20 +0100

    reworked styling of radio buttons

--- src/components/layouts/public/Orders/Cart/CartRadioGroup/CartRadioGroup.js
+++ src/components/layouts/public/Orders/Cart/CartRadioGroup/CartRadioGroup.js
@@ -8,12 +8,33 @@ import RadioGroup from '../../../../../common/Forms/RadioGroup';
 
 import { DELIVERY, PICKUP } from '../../../../../../utils/constant';
 
-const CartRadioGroupContainer = styled(RadioGroup)`
-  display: flex;
-  border-top: solid 1px var(--color-tertiary);
-  padding: 0.2rem 0;
+const CardRadioGroupContainer = styled(RadioGroup)`
+  padding-bottom: 0;
   label {
-    margin-left: 1rem;
+    margin-bottom: 0;
+    border-radius: 0;
+    border-bottom: 0;
+    border-left: 0;
+    border-right: 0;
+
+    @media only screen and (min-width: 931px) {
+      width: 50%;
+    }
+
+    &:nth-child(2) {
+      /* border-bottom-left-radius: 7px; */
+      @media only screen and (min-width: 931px) {
+        border-bottom-left-radius: 7px;
+      }
+    }
+
+    &:last-child {
+      border-bottom-left-radius: 7px;
+      border-bottom-right-radius: 7px;
+      @media only screen and (min-width: 931px) {
+        border-bottom-left-radius: 0px;
+      }
+    }
   }
 `;
 
@@ -25,29 +46,27 @@ const CartRadioGroup = ({ orderMethod }) => {
   }, [selectedMethod, orderMethod]);
 
   return (
-    <CartRadioGroupContainer>
-      <label>
-        <input
-          type="radio"
-          value={DELIVERY}
-          name="deliveryGroup"
-          checked={selectedMethod === DELIVERY}
-          onChange={(event) => setSelectedMethod(event.target.value)}
-        />
-        Doručit na adresu
-      </label>
+    <CardRadioGroupContainer>
+      <input
+        id="deliveryId"
+        type="radio"
+        value={DELIVERY}
+        name="deliveryGroup"
+        checked={selectedMethod === DELIVERY}
+        onChange={(event) => setSelectedMethod(event.target.value)}
+      />
+      <label htmlFor="deliveryId">Doručit na adresu</label>
 
-      <label>
-        <input
-          type="radio"
-          value={PICKUP}
-          name="deliveryGroup"
-          checked={selectedMethod === PICKUP}
-          onChange={(event) => setSelectedMethod(event.target.value)}
-        />
-        Vyzvednu osobně
-      </label>
-    </CartRadioGroupContainer>
+      <input
+        id="pickupId"
+        type="radio"
+        value={PICKUP}
+        name="deliveryGroup"
+        checked={selectedMethod === PICKUP}
+        onChange={(event) => setSelectedMethod(event.target.value)}
+      />
+      <label htmlFor="pickupId">Vyzvednu osobně</label>
+    </CardRadioGroupContainer>
   );
 };
 
