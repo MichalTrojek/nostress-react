7073c56cd742ac2559204555c01dcf7e6ea946b6
MichaTrojek <MichalTrojek1@gmail.com>
2021-01-12 19:44:06 +0100

    styling

--- src/components/layouts/public/Orders/MealsSelector/Menus/MealListContainer/MealListContainer.js
+++ src/components/layouts/public/Orders/MealsSelector/Menus/MealListContainer/MealListContainer.js
@@ -4,6 +4,7 @@ const MealListContainer = styled.div`
   display: grid;
   grid-gap: 1rem;
   grid-template-columns: repeat(auto-fill, minmax(34rem, 1fr));
+  grid-auto-rows: 24rem;
 `;
 
 export default MealListContainer;
