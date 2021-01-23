e596dd1563c7d7e2f6047dd848a21f84c1e6c4ab
MichaTrojek <MichalTrojek1@gmail.com>
2021-01-23 17:18:12 +0100

    refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -51,7 +51,6 @@ function App() {
             path="/dashboard/cards"
             component={NewsCardPage}
           />
-
           <Route component={NotFound} />
         </Switch>
       </AuthProvider>
