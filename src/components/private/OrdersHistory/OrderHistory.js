6020304e9593cf972d252fc956b1ec43ea6b19f6
MichaTrojek <MichalTrojek1@gmail.com>
2021-02-07 21:11:35 +0100

    added OrderHistory skeleton

---
+++ src/components/private/OrdersHistory/OrderHistory.js
@@ -0,0 +1,14 @@
+import Background from '../../common/Background';
+import Wrapper from '../../common/Wrapper';
+
+const OrderHistory = () => {
+  return (
+    <Background>
+      <Wrapper>
+        <h1>Historie Objednávek</h1>
+      </Wrapper>
+    </Background>
+  );
+};
+
+export default OrderHistory;
