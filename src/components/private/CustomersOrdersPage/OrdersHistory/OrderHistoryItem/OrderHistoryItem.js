780779be12eeaf1a97eb285044927323455ef5bc
MichaTrojek <MichalTrojek1@gmail.com>
2021-02-08 13:21:20 +0100

    added nubmer of order

--- src/components/private/CustomersOrdersPage/OrdersHistory/OrderHistoryItem/OrderHistoryItem.js
+++ src/components/private/CustomersOrdersPage/OrdersHistory/OrderHistoryItem/OrderHistoryItem.js
@@ -8,13 +8,23 @@ const OrderHistoryItemContainer = styled.div`
   position: relative;
   border-bottom-left-radius: 10px;
 
-  .detailButton {
+  .OrderhistoryItem__number {
+    position: absolute;
+    left: 0;
+    top: 0;
+    padding: 0.7rem;
+    height: 100%;
+    border-bottom-left-radius: 7px;
+    background-color: var(--color-tertiary);
+    color: black;
+  }
+
+  .OrderhistoryItem_detailButton {
     top: 0;
     right: 0;
     position: absolute;
     border-radius: 0;
     height: 100%;
-
     &:hover {
       transform: translateY(0);
     }
@@ -24,10 +34,13 @@ const OrderHistoryItemContainer = styled.div`
 const OrderHistoryItem = () => {
   return (
     <OrderHistoryItemContainer>
-      <p>item</p>
-      <p>Číslo objednávka</p>
-      <p>testě</p>
-      <Button className="detailButton" primary>
+      <div className="OrderhistoryItem__number">
+        <p> Objednávka číslo: 25</p>
+      </div>
+
+      {/* <p>Číslo objednávka</p>
+      <p>testě</p> */}
+      <Button className="OrderhistoryItem_detailButton" primary>
         detail
       </Button>
     </OrderHistoryItemContainer>
