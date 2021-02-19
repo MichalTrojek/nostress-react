72bde688d8e5163ceb80a13d5931be9b2af12f4a
MichaTrojek <MichalTrojek1@gmail.com>
2021-02-19 23:50:25 +0100

    prepared UI for searching orders by email

--- src/components/private/CustomersOrdersPage/OrdersHistory/OrderHistory.js
+++ src/components/private/CustomersOrdersPage/OrdersHistory/OrderHistory.js
@@ -4,6 +4,8 @@ import Background from '../../../common/Background';
 import Wrapper from '../../../common/Wrapper';
 import OrderHistoryItem from './OrderHistoryItem';
 
+import FormGroup from '../../../common/Forms/FormGroup';
+
 import Button from '../../../common/Button';
 
 import { fetchLastOrderNumber, fetchPage } from './api/pagination';
@@ -26,10 +28,16 @@ const HistoryButtonsContainer = styled.div`
   }
 `;
 
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const OrderHistory = () => {
   const [page, setPage] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [showSearch, setShowSearch] = useState(0);
+  const [showSearch, setShowSearch] = useState(false);
+  const [email, setEmail] = useState('');
   const [maxSize, setMaxSize] = useState(0);
   const PAGE_SIZE = 10;
 
@@ -44,21 +52,51 @@ const OrderHistory = () => {
   return (
     <Background>
       <Wrapper>
-        <HistoryButtonsContainer>
-          <Button primary onClick={handleNewerButton}>
-            Novější
-          </Button>
-          <Button primary onClick={handleSearchButton}>
-            Vyhledavaní
-          </Button>
-          <Button primary onClick={handleOlderButton}>
-            Starší
-          </Button>
-        </HistoryButtonsContainer>
+        {showSearch ? renderSearch() : renderHistoryButtons()}
         <OrderHistoryItemsList>{renderPage()}</OrderHistoryItemsList>
       </Wrapper>
     </Background>
   );
+
+  function renderSearch() {
+    return (
+      <SearchContainer>
+        <FormGroup>
+          <input
+            type="text"
+            placeholder="Hledat email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            id="searchEmailInput"
+          />
+          <label htmlFor="searchEmailInput">Hledat email</label>
+        </FormGroup>
+        <Button primary onClick={handleSearchEmailButton} Vyhledat email>
+          Vyhledat
+        </Button>
+      </SearchContainer>
+    );
+  }
+
+  function handleSearchEmailButton() {
+    console.log('TET');
+  }
+
+  function renderHistoryButtons() {
+    return (
+      <HistoryButtonsContainer>
+        <Button primary onClick={handleNewerButton}>
+          Novější
+        </Button>
+        <Button primary onClick={handleSearchButton}>
+          Vyhledavaní
+        </Button>
+        <Button primary onClick={handleOlderButton}>
+          Starší
+        </Button>
+      </HistoryButtonsContainer>
+    );
+  }
 
   function handleSearchButton() {
     setShowSearch(!showSearch);
